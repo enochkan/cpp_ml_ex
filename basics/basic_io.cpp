401ad59ba09cf6acde814196b3f39f4935afb982
Enoch Kan <kanxx030@gmail.com>
2021-03-16 00:11:23 +0000

    basic io practice

---
+++ basics/basic_io.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // declare num as type int
+    int num;
+    //takes an input as num
+    cout << "input num: ";
+    cin >> num;
+    cout << "the number you entered is: " << num;
+    return 0;
+}
